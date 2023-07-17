aeff45004b605709ac76acea8e05775b487a3136
3eonah <3eonah.lee@gmail.com>
2023-07-17 23:27:42 +0900

    프론트엔드 3주차 세션실습&과제

---
+++ likelion_frontend/assignments/3rd-week/3rd-week/src/APage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const APage = () => {
+    const name="멋사 🦁";
+    return (
+        <div>
+            <p>A 페이지: name={name}</p>
+        </div>
+    );
+};
+
+export default APage;
---
+++ likelion_frontend/sessions/3rd-session/src/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = () => {
+    return (
+        <div>
+            하이루..루..ㄹ..룰루랄라..
+        </div>
+    );
+};
+
+export default Hello;
